31b086fbd8e78baef07f8935619e2cf3c98d7098
Miguel Perello garcia <canyavall@gmail.com>
2017-10-18 07:28:39 +0200

    isim stand by

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
 import {BrowserRouter as  Router, Route, Switch  } from 'react-router-dom';
-import store from './service/store'
+import store from './service/store';
 
 //Routes
 import Home from './routes/Home';
 
+//Main code
 ReactDOM.render(
     <Provider store = {store}>
         <Router>
